0997717b29c41c0c1ada5de3f7e15146c1f2a271
GetJumpedOn95 <brayden.edington@student.allenisd.org>
2023-05-09 20:09:55 -0500

    stuff

--- W2654_b/Sources/ScenesShell/Background.swift
+++ W2654_b/Sources/ScenesShell/Background.swift
@@ -10,10 +10,15 @@ import Igis
 class Background : RenderableEntity {
     let mapImage : Image
     init() {
-            guard let mapURL = URL(string:"/home/brayden-edington/Experiences/W2654_b/Sources/ScenesShell/map.jpeg") else {
-                            fatalError("Failed to create URL for whitehouse")
-                                    }
-                                            mapImage = Image(sourceURL:mapURL)
+          //  guard let mapURL = URL(string:"/home/brayden-edington/Experiences/W2654_b/Sources/ScenesShell/map.jpeg") else {
+          //                  fatalError("Failed to create URL for whitehouse")
+ guard let mapUrl = Bundle.main.url(
+            forResource: "map",
+            withExtension: "json"
+        ) else {
+            throw Error.fileNotFound(name: name)
+        }
+        mapImage = Image(sourceURL:mapURL)
 
           // Using a meaningful name can be helpful for debugging
           super.init(name:"Background")
