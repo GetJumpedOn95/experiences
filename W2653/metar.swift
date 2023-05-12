3424b511fc1e5d04621882c9cef52c3175b9f489
Brayden Edington <brayden.edington@student.allenisd.org>
2023-05-12 09:35:31 -0500

    metar

--- W2653/metar.swift
+++ W2653/metar.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Swift
+import CoreFoundation
 
 
 struct Metar {
@@ -48,8 +49,25 @@ struct Metar {
     var vert_vis_ft: Float?
     var metar_type: String?
     var elevation_m: Float?
-
 }
+    func ConvertToMetar(components:[String]) -> Metar {
+        let metar: Metar = Metar(raw_text:components[0], station_id:components[1], observation_time:components[2],
+                                 latitude:Float(components[3]), longitude:Float(components[4]), temp_c:Float(components[5]), dewpoint_c:Float(components[6]),
+                                 wind_dir_degrees:Float(components[7]), wind_speed_kt:Float(components[8]), wind_gust_kt:Float(components[9]), 
+                                 visibility_statute_mi:Float(components[10]), altim_in_hg:Float(components[11]), sea_level_pressure_mb:Float(components[12]),
+                                 corrected:Bool(components[13]),auto: Bool(components[14]), auto_station: Bool(components[15]),
+                                 maintenance_indicator_on:Bool(components[16]), no_signal:Bool(components[17]), lightning_sensor_off:Bool(components[18]),
+                                 freezing_rain_sensor_off:Bool(components[19]), present_weather_sensor_off:Bool(components[20]),
+                                 wx_string:components[21], sky_cover:Float(components[22]), cloud_base_ft_agl:Float(components[23]), sky_cover2:Float(components[24]),
+                                 cloud_base_ft_agl2:Float(components[25]), sky_cover3:Float(components[26]), cloud_base_ft_agl3:Float(components[27]),
+                                 sky_cover4:Float(components[28]), cloud_base_ft_agl4:Float(components[29]), flight_category:components[30],
+                                 three_hr_pressure_tendency_mb:Float(components[31]), maxT_c:Float(components[32]), minT_c:Float(components[33]), maxT24hr_c:Float(components[34]),
+                                 minT24hr_c:Float(components[35]), precip_in:Float(components[36]), pcp3hr_in:Float(components[37]), pcp6hr_in:Float(components[38]),
+                                 pcp24hr_in:Float(components[39]), snow_in:Float(components[40]), vert_vis_ft:Float(components[41]), metar_type:components[42],
+                                 elevation_m:Float(components[43]))
+        return metar
+    }
+
 
 extension Metar: CustomStringConvertible {
     var description: String {
